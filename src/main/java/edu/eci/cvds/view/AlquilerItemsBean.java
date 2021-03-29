d60b7813b397d9f1cc20470f945116098c0e3c28
AngieMeG <70661112+AngieMeG@users.noreply.github.com>
2021-03-29 01:17:04 -0500

    Desarrollador 1: Vista Cliente Final

--- src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
+++ src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -21,7 +21,53 @@ public class AlquilerItemsBean extends BasePageBean {
     @Inject
     private ServiciosAlquiler serviciosAlquiler;
     private Cliente cliente;
+    private List<Cliente> added;
     private long costoAlquiler;
+
+    /**
+     * Retorna una lista con la informacion de los clientes almacenados en la base de datos
+     * @return todos los clientes de la base de datos
+     * @throws ExcepcionServiciosAlquiler
+     */
+    public AlquilerItemsBean(){
+	added = new ArrayList<Cliente>();
+	costoAlquiler=0;
+    }
+
+
+
+    public List<Cliente> consultarClientes() throws ExcepcionServiciosAlquiler{
+        try {
+	    List<Cliente> clientes = new ArrayList<Cliente>();
+	    clientes.addAll(added);
+	    clientes.addAll(serviciosAlquiler.consultarClientes());
+            return clientes;   
+        } catch (Exception e) {
+            throw new ExcepcionServiciosAlquiler("Error al consultar la tabla Clientes");
+        }
+    }
+
+    /**
+      * Registra un cliente nuevo en la base de datos
+      * @param Nombre nombre del nuevo cliente
+      * @param Documeto documento de identidad del nuevo cliente
+      * @param Telefono telefono del nuevo cliente
+      * @param Direccion direccion del nuevo cliente
+      * @param Email email del nuevo cliente
+      * 
+      */
+    public void registrar(String nombre, long documento, String telefono, String direccion, String email) throws ExcepcionServiciosAlquiler{
+        added.add(new Cliente(nombre,documento,telefono,direccion,email));
+	serviciosAlquiler.registrarCliente(added.get(added.size() - 1));
+    }
+
+    public void setCliente(Cliente cliente){
+	this.cliente = cliente;
+    }
+    
+    public Cliente getCliente(){
+	return this.cliente;
+    }
 
     /**
      * Retorna una lista con la informacion de los items rentados por un cliente dado
